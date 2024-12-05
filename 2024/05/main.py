35400a7f975045de1a80cdd52b792bed8d878751
rswrz <git@rswrz.net>
2024-12-05 20:26:22 +0100

    Day 5
    Signed-off-by: rswrz <git@rswrz.net>

---
+++ 2024/05/main.py
@@ -0,0 +1,21 @@
+def valid_page_order(update, rules):
+    for i, page in enumerate(update):
+        for before, after in rules:
+            if page == before and i > update.index(after) or page == after and i < update.index(before):
+                return False
+    return True
+
+
+input = open("input.txt").read().splitlines()
+rules = [list(map(int, line.split("|"))) for line in input if "|" in line]
+updates = [list(map(int, line.split(","))) for line in input if "," in line]
+
+result = 0
+for update in updates:
+    update_rules = [rule for rule in rules if all(r in update for r in rule)]
+
+    if valid_page_order(update, update_rules):
+        middle_page_index = int((len(update) - 1) / 2)
+        result += update[middle_page_index]
+
+print(result)
---
+++ 2024/05/part2.py
@@ -0,0 +1,37 @@
+def valid_page_order(update, rules):
+    for i, page in enumerate(update):
+        for before, after in rules:
+            if page == before and i > update.index(after) or page == after and i < update.index(before):
+                return False
+    return True
+
+
+def reorder_pages(update, rules):
+    while not valid_page_order(update, rules):
+        for i, page in enumerate(update):
+            for before, after in rules:
+                if page == before:
+                    j = update.index(after)
+                    if i > j:
+                        update[i], update[j] = update[j], update[i]
+                if page == after:
+                    j = update.index(before)
+                    if i < j:
+                        update[i], update[j] = update[j], update[i]
+
+
+input = open("input.txt").read().splitlines()
+rules = [list(map(int, line.split("|"))) for line in input if "|" in line]
+updates = [list(map(int, line.split(","))) for line in input if "," in line]
+
+result = 0
+for update in updates:
+    update_rules = [rule for rule in rules if all(r in update for r in rule)]
+
+    if not valid_page_order(update, update_rules):
+        reorder_pages(update, update_rules)
+
+        middle_page_index = int((len(update) - 1) / 2)
+        result += update[middle_page_index]
+
+print(result)
